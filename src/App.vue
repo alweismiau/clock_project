982edd4b97d5390a04ed94534b8491fe86f432d3
alweismiau <keizafina@gmail.com>
2022-09-25 19:18:34 +0700

    clock

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <DigitalClock/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import DigitalClock from "./components/DigitalClock.vue"
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    DigitalClock
   }
 }
 </script>
 
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+@font-face {
+  font-family: "alarm clock";
+  src: url(../public/fonts/alarm.ttf);
 }
 </style>
---
+++ src/components/DigitalClock.vue
@@ -0,0 +1,52 @@
+<template>
+  <h1>Digital Clock</h1>
+  <p>Fina Keiza Arismana</p>
+  <div class="container">
+    <div class="LCD">
+      <div class="hours">{{ hours }}</div>
+      <div class="divider">:</div>
+      <div class="minutes">{{ minutes }}</div>
+      <div class="divider">:</div>
+      <div class="seconds">{{ seconds }}</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "DigitalClock",
+  data() {
+    return {
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+  },
+  mounted() {
+    setInterval(() => this.setTime(), 1000);
+  },
+  methods: {
+    setTime() {
+      const date = new Date();
+      let hours = date.getHours();
+      let minutes = date.getMinutes();
+      let seconds = date.getSeconds();
+      hours = hours <= 9 ? `${hours}`.padStart(2, 0) : hours;
+      minutes = minutes <= 9 ? `${minutes}`.padStart(2, 0) : minutes;
+      seconds = seconds <= 9 ? `${seconds}`.padStart(2, 0) : seconds;
+      this.hours = hours;
+      this.minutes = minutes;
+      this.seconds = seconds;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.LCD {
+  display: inline-flex;
+}
+.LCD > div {
+  font-family: "alarm clock";
+  font-size: xx-large;
+}
+</style>
